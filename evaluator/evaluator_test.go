be4114ced8828e7b93723e2a66e5362dd9f0a655
若帆 叶 <ruofan.ye@gu3.co.jp>
2018-07-05 09:44:38 +0900

    add errorObj and mortem-able test

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -112,6 +112,41 @@ func TestRetrunStatement(t *testing.T) {
 	}
 }
 
+func TestErrorHandling(t *testing.T) {
+	tests := []struct {
+		input    string
+		eMessage string
+	}{
+		{"5 + true", "type mismatch: INTEGER + BOOLEAN"},
+		{"5 + true; 5;", "type mismatch: INTEGER + BOOLEAN"},
+		{"-true", "unknown operator: -BOOLEAN"},
+		{"true + false", "unknown operator: BOOLEAN + BOOLEAN"},
+		{"5; true + false; 5", "unknown operator: BOOLEAN + BOOLEAN"},
+		{"if (10 > 1) { true + false; }", "unknown operator: BOOLEAN + BOOLEAN"},
+		{`
+		if (10 > 1){
+			if (10 > 1){
+				return true + false;
+			}
+			return 1;
+		}`, "unknown operator: BOOLEAN + BOOLEAN"},
+	}
+
+	for _, tt := range tests {
+		e := testEval(tt.input)
+
+		err, ok := e.(*object.Error)
+		if !ok {
+			t.Errorf("no error object returned. got=%T(%+v)", e, e)
+			continue
+		}
+
+		if err.Message != tt.eMessage {
+			t.Errorf("wrong error message. expected=%q, got=%q", tt.eMessage, err.Message)
+		}
+	}
+}
+
 func TestBangOperator(t *testing.T) {
 	tests := []struct {
 		input    string
